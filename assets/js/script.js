d4aa2f12b5a236db689b7da5e1dcbc5f8f00e2df
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-08 13:46:42 -0400

    set current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
 var today = moment().format('MMMM Do, YYYY');
 $('#currentDay').text("Today is " + today)
+
+
+var current_time = new Date();
+hour = current_time.getHours();
+console.log(typeof hour)
+
+    if (hour = 14){
+        $('.hour14').addClass("present")
+    } else if (hour > 14){
+        $('.hour14').addClass("future")
+    } else {
+        $('.hour14').addClass("past")
+    };
+
+    if (hour = 13){
+        $('.hour13').addClass("present")
+    } else if (hour > 13){
+        $('.hour13').addClass("future")
+    } else {
+        $('.hour13').addClass("past")
+    };
+
+
+
