f737c7a4163ab11e3b8320700e0ac79f342c9d57
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-08 15:55:16 -0400

    Color coded based on time complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,34 @@
+const timeSlot = document.getElementsByClassName("schedule");
+let hour = parseInt(moment().format('H'));
 var today = moment().format('MMMM Do, YYYY');
-$('#currentDay').text("Today is " + today)
+$('#currentDay').text("Today is " + today);
 
+Array.from(timeSlot).forEach(schedule => {
+    let 
+        scheduleIdString = schedule.id,
+        scheduleHour;
+    if (scheduleIdString) {
+        scheduleHour = parseInt(scheduleIdString);
+    }
+    if (scheduleHour) {
+        if (hour == scheduleHour) {
+           $(schedule).addClass("present");
+        } else if (hour < scheduleHour){
+            $(schedule).addClass("future")
+        } else {
+            $(schedule).addClass("past")
+        }
+    }
+})
 
-var current_time = new Date();
-hour = current_time.getHours();
-console.log(typeof hour)
+function setCurrent(element, current) {
+    element.style.classList.add = current;
+}
 
-    if (hour = 14){
-        $('.hour14').addClass("present")
-    } else if (hour > 14){
-        $('.hour14').addClass("future")
-    } else {
-        $('.hour14').addClass("past")
-    };
+function setFuture() {
+    $('.schedule').addClass("future");
+}
 
-    if (hour = 13){
-        $('.hour13').addClass("present")
-    } else if (hour > 13){
-        $('.hour13').addClass("future")
-    } else {
-        $('.hour13').addClass("past")
-    };
-
-
-
+function setPast() {
+    $('.col-10').addClass("past");
+}
