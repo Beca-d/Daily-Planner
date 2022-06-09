9cc1ed7462ac76274c16484ec48c34ec270e4d5d
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-06-08 22:42:48 -0400

    updated comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// Getting variables to set date and style based on time
 const timeSlot = document.getElementsByClassName("schedule");
 let hour = parseInt(moment().format('H'));
 var today = moment().format('MMMM Do, YYYY');
 $('#currentDay').text("Today is " + today);
 
+//Conditional statement to set background colour of time slot based on current hour
 Array.from(timeSlot).forEach(schedule => {
     let 
         scheduleIdString = schedule.id,
@@ -21,12 +23,14 @@ Array.from(timeSlot).forEach(schedule => {
     }
 });
 
+// Saving input text to local storage
   function save_data(e){
     var number = $(e).data('num');
     var input = document.getElementById('textArea' + number).value;
     localStorage.setItem('text' + number, input);
   };
   
+  //retrieving appointments from local storage 
   for(var i = 6; i <= 19; i++){
     document.getElementById('textArea' + i).value = localStorage.getItem('text' + i);
   };
